a515e12081c095c00cf4134fef5e5df7cfb8a59b
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-23 22:18:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function displayBrowserInfo() {
+    var browserName = navigator.appName;
+    var version = navigator.appVersion;
+
+    var browserInfoMessage = "You are using " + browserName + " version " + version;
+    var browserInfoDiv = document.getElementById('browser-info');
+    if (browserInfoDiv) {
+      browserInfoDiv.innerHTML = browserInfoMessage;
+    }
+  }
+
+  // Call the function when the page is loaded
+  window.onload = displayBrowserInfo;
